b644d2cbf04ca6887d916bc151134310cb776aab
Freeman Liu <llw599502537@gmail.com>
2025-12-26 02:25:19 +0800

    done

--- idea-plugin/src/main/java/recordbuilder/idea/RecordBuilderElementFinder.java
+++ idea-plugin/src/main/java/recordbuilder/idea/RecordBuilderElementFinder.java
@@ -3,16 +3,23 @@ package recordbuilder.idea;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiClassType;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiElementFinder;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiModifier;
 import com.intellij.psi.PsiPackage;
+import com.intellij.psi.PsiRecordComponent;
+import com.intellij.psi.PsiTypes;
+import com.intellij.psi.impl.light.LightMethodBuilder;
 import com.intellij.psi.impl.light.LightPsiClassBuilder;
 import com.intellij.psi.search.GlobalSearchScope;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -103,7 +110,100 @@ public class RecordBuilderElementFinder extends PsiElementFinder {
         var builder = new RecordBuilderLightClass(recordClass, builderClassName, qualifiedName);
         builder.getModifierList().addModifier(PsiModifier.PUBLIC);
         builder.getModifierList().addModifier(PsiModifier.FINAL);
+        for (var method : createBuilderMethods(recordClass, builder)) {
+            builder.addMethod(method);
+        }
         return builder;
+    }
+
+    private static List<PsiMethod> createBuilderMethods(PsiClass recordClass, PsiClass builderClass) {
+        List<PsiMethod> methods = new ArrayList<>();
+        PsiElementFactory factory = JavaPsiFacade.getElementFactory(recordClass.getProject());
+        PsiClassType builderType = factory.createType(builderClass);
+        PsiClassType recordType = factory.createType(recordClass);
+
+        // setter: setXxx methods
+        for (PsiRecordComponent component : recordClass.getRecordComponents()) {
+            String methodName = "set" + RecordBuilderUtils.capitalize(component.getName());
+            LightMethodBuilder setter = new LightMethodBuilder(recordClass.getManager(), methodName);
+            setter.setModifiers(PsiModifier.PUBLIC);
+            setter.setContainingClass(builderClass);
+            setter.addParameter(component.getName(), component.getType());
+            setter.setMethodReturnType(builderType);
+            setter.setNavigationElement(component);
+            methods.add(setter);
+        }
+
+        // hasXxx methods
+        for (PsiRecordComponent component : recordClass.getRecordComponents()) {
+            String methodName = "has" + RecordBuilderUtils.capitalize(component.getName());
+            LightMethodBuilder hasMethod = new LightMethodBuilder(recordClass.getManager(), methodName);
+            hasMethod.setModifiers(PsiModifier.PUBLIC);
+            hasMethod.setContainingClass(builderClass);
+            hasMethod.setMethodReturnType(PsiTypes.booleanType());
+            hasMethod.setNavigationElement(component);
+            methods.add(hasMethod);
+        }
+
+        // getXxx methods
+        for (PsiRecordComponent component : recordClass.getRecordComponents()) {
+            String methodName = "get" + RecordBuilderUtils.capitalize(component.getName());
+            LightMethodBuilder getMethod = new LightMethodBuilder(recordClass.getManager(), methodName);
+            getMethod.setModifiers(PsiModifier.PUBLIC);
+            getMethod.setContainingClass(builderClass);
+            getMethod.setMethodReturnType(component.getType());
+            getMethod.setNavigationElement(component);
+            methods.add(getMethod);
+        }
+
+        // clearXxx methods
+        for (PsiRecordComponent component : recordClass.getRecordComponents()) {
+            String methodName = "clear" + RecordBuilderUtils.capitalize(component.getName());
+            LightMethodBuilder clearMethod = new LightMethodBuilder(recordClass.getManager(), methodName);
+            clearMethod.setModifiers(PsiModifier.PUBLIC);
+            clearMethod.setContainingClass(builderClass);
+            clearMethod.setMethodReturnType(builderType);
+            clearMethod.setNavigationElement(component);
+            methods.add(clearMethod);
+        }
+        // clear() method
+        LightMethodBuilder clearMethod = new LightMethodBuilder(recordClass.getManager(), "clear");
+        clearMethod.setModifiers(PsiModifier.PUBLIC);
+        clearMethod.setContainingClass(builderClass);
+        clearMethod.setMethodReturnType(builderType);
+        methods.add(clearMethod);
+
+        // merge(Record) method
+        LightMethodBuilder mergeMethod = new LightMethodBuilder(recordClass.getManager(), "merge");
+        mergeMethod.setModifiers(PsiModifier.PUBLIC);
+        mergeMethod.setContainingClass(builderClass);
+        mergeMethod.addParameter("other", recordType);
+        mergeMethod.setMethodReturnType(builderType);
+        methods.add(mergeMethod);
+
+        // builder() static method (on Builder class)
+        LightMethodBuilder builderMethod = new LightMethodBuilder(recordClass.getManager(), "builder");
+        builderMethod.setModifiers(PsiModifier.PUBLIC, PsiModifier.STATIC);
+        builderMethod.setContainingClass(builderClass);
+        builderMethod.setMethodReturnType(builderType);
+        methods.add(builderMethod);
+
+        // builder(Record) static method (on Builder class)
+        LightMethodBuilder builderFromSource = new LightMethodBuilder(recordClass.getManager(), "builder");
+        builderFromSource.setModifiers(PsiModifier.PUBLIC, PsiModifier.STATIC);
+        builderFromSource.setContainingClass(builderClass);
+        builderFromSource.addParameter("prototype", recordType);
+        builderFromSource.setMethodReturnType(builderType);
+        methods.add(builderFromSource);
+
+        // build() method
+        LightMethodBuilder buildMethod = new LightMethodBuilder(recordClass.getManager(), "build");
+        buildMethod.setModifiers(PsiModifier.PUBLIC);
+        buildMethod.setContainingClass(builderClass);
+        buildMethod.setMethodReturnType(recordType);
+        methods.add(buildMethod);
+
+        return methods;
     }
 
     private static class RecordBuilderLightClass extends LightPsiClassBuilder {
